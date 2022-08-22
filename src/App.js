0cbac7a5f3d392fbf3c5d0cf2eb657962c0bc9f7
TedWang06 <111414537+TedWang06@users.noreply.github.com>
2022-08-22 21:24:43 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import {
   NavLink,
 } from 'react-router-dom';
 
+
+const Logout = () => {
+  const navigate = useNavigate()
+  return (
+    <button onClick={() => navigate('/login')} >
+      Logout
+    </button>
+  )
+}
+
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return <>
+      <p>這是 Todo 頁面 
+      </p>
+      <Logout />
+  </>;
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
